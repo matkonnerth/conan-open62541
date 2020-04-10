3284ac1b98c30317849f1f39c73255a5b3f5ee6b
matkonnerth <matkonnerth@gmail.com>
2020-04-10 19:23:18 +0200

    fix conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class Open62541Conan(ConanFile):
     name = "open62541"
-    version = "0.4"
+    version = "1.0"
     license = "Mozilla Public License v2.0"
     author = "matkonnerth@gmail.com"
     url = "https://github.com/matkonnerth/conan-open62541"
@@ -18,26 +18,23 @@ class Open62541Conan(ConanFile):
     def source(self):
         self.run("git clone https://github.com/open62541/open62541.git")
         self.run("cd open62541 && git checkout master")
-        self.run("cd open62541 && git submodule init")
-        self.run("cd open62541 && git submodule update")
+        #self.run("git submodule init")
+        #self.run("git submodule update")
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        #true
-        cmake.definitions['UA_ENABLE_PUBSUB'] = True
-        cmake.definitions['UA_ENABLE_PUBSUB_INFORMATIONMODEL'] = True
-        cmake.definitions['UA_ENABLE_PUBSUB_INFORMATIONMODEL_METHODS'] = True
-        cmake.definitions['UA_NAMESPACE_ZERO'] = "FULL"
-
+        cmake.definitions["UA_NAMESPACE_ZERO"] = "MINIMAL"
+        cmake.definitions["CMAKE_BUILD_TYPE"] = "Debug"
+        cmake.configure(source_folder="open62541")
         return cmake
 
     def build(self):
         cmake = self._configure_cmake()
-        cmake.configure(source_folder="open62541")
         cmake.build()
 
     def package(self):
         cmake = self._configure_cmake()
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = "./"
         cmake.install()
         cmake.patch_config_paths()
 
