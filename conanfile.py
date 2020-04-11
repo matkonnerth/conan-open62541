4d5bee4ba62184133a911c35c41388c0d379916f
matkonnerth <matkonnerth@gmail.com>
2020-04-11 09:04:59 +0200

    try to fix clang build

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,10 @@
 from conans import ConanFile, CMake, tools
 
-
 class Open62541Conan(ConanFile):
     name = "open62541"
-    version = "latest"
+    version = "1.0.1"
     license = "Mozilla Public License v2.0"
-    author = "matkonnerth@gmail.com"
-    url = "https://github.com/matkonnerth/conan-open62541"
+    url = "https://github.com/open62541/open62541"
     homepage = "https://open62541.org/"
     description = "open source C99 implementation of OPC UA"
     topics = ("conan", "opcua", "open62541")
@@ -16,17 +14,14 @@ class Open62541Conan(ConanFile):
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/open62541/open62541.git")
-        self.run("cd open62541 && git checkout master")
-        self.run("cd open62541 && git submodule init")
-        self.run("cd open62541 && git submodule update")
+        tools.get(**self.conan_data["sources"][self.version])
 
     def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["CMAKE_BUILD_TYPE"] = "Debug"
-        cmake.definitions["UA_NAMESPACE_ZERO"] = "FULL"
+        cmake.definitions["UA_NAMESPACE_ZERO"] = "MINIMAL"
         cmake.definitions["UA_ENABLE_SUBSCRIPTIONS_EVENTS"] = "ON"
-        cmake.configure(source_folder="open62541")
+        cmake.configure(source_folder="open62541-" +self.version)
         return cmake
 
     def build(self):
