8420684a257de5a7ed0b26f88678467b58632c05
matkonnerth <matkonnerth@gmail.com>
2020-04-11 11:34:54 +0200

    build only debug

--- build.py
+++ build.py
@@ -3,5 +3,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
+                options={}, env_vars={}, build_requires={})   
     builder.run()
