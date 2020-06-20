79a7ad6b9236defe1913ebcb13d551508532f251
mkonnerth <matkonnerth@gmail.com>
2020-06-20 07:11:40 +0200

    use RelWithDebInfo

--- build.py
+++ build.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
                 options={}, env_vars={}, build_requires={})
-    builder.add(settings={"arch": "x86_64", "build_type": "Release"},
+    builder.add(settings={"arch": "x86_64", "build_type": "RelWithDebInfo"},
                 options={}, env_vars={}, build_requires={})   
     builder.run()
